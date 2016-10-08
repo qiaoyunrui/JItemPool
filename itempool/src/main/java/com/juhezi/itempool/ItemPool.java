090eee3c604529ce9bc65995103815ad7a178e42
Juhezi <juhezix@163.com>
2016-10-08 09:19:26 +0800

    init commit

---
+++ itempool/src/main/java/com/juhezi/itempool/ItemPool.java
@@ -0,0 +1,14 @@
+package com.juhezi.itempool;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by qiao1 on 2016/10/8.
+ */
+public class ItemPool {
+
+    private static String TAG = "ItemPool";
+    private static final AtomicInteger ID_BASE = new AtomicInteger(0);
+
+
+}
